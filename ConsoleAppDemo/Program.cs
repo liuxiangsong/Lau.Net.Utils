82d7939964b22ac1d0e218e287bc94662cccaac1
Lau <pacid@foxmail.com>
2024-07-13 18:50:37 +0800

    测试

--- ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/Program.cs
@@ -1,8 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleAppDemo;
+using Lau.Net.Utils.Net;
+using RestSharp;
 
 //ConfigureService.Configure();
 
 Console.WriteLine("Hello, World!");
 
+using (var httpClient = new HttpClient())
+{
+    var response = await httpClient.GetAsync("https://localhost:7027/WeatherForecast/Stream", HttpCompletionOption.ResponseHeadersRead);
 
+    if (response.IsSuccessStatusCode)
+    {
+        using (var stream = await response.Content.ReadAsStreamAsync())
+        using (var reader = new System.IO.StreamReader(stream))
+        {
+            while (!reader.EndOfStream)
+            {
+                var result = await reader.ReadLineAsync();
+                Console.WriteLine(result);
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Request failed with status code: {response.StatusCode}");
+    }
+}
