71cba8c2bccb1bfe7b6f56e654912bb593c59749
Lau <pacid@foxmail.com>
2024-11-19 09:55:15 +0800

    JsonTest添加测试用例

--- Lau.Net.Utils.Tests/JsonTest.cs
+++ Lau.Net.Utils.Tests/JsonTest.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -22,15 +23,76 @@ namespace Lau.Net.Utils.Tests
                     new StringOrNullToNumberConverter<decimal>(),
                     new StringOrNullToNumberConverter<int>()
                 }
-            }; 
+            };
             var entities = JsonConvert.DeserializeObject<List<MyEntity>>(jsonString, settings);
-            Assert.IsNotNull(entities); 
+            Assert.IsNotNull(entities);
         }
-    }
 
-    public class MyEntity
-    {
-        public decimal Prop1 { get; set; }
-        public int Prop2 { get; set; }
+        [Test]
+        public void CreateJobjectTest()
+        {
+            var param = new JObject
+            {
+                ["userid"] = "123",
+                ["update_items"] = new JArray
+                    {
+                        new JObject
+                        {
+                            ["fieldid"] = 12024,
+                            ["value_string"] = "xxx"
+                        },
+                        new JObject
+                        {
+                            ["fieldid"] = 12025,
+                            ["value_string"] = "yyy"
+                        }
+                    }
+            };
+            var json = JsonConvert.SerializeObject(param);
+            Console.WriteLine(json);
+        }
+
+        [Test]
+        public void MergeJobjectTest()
+        {
+            var param1 = new JObject
+            {
+                ["userid"] = "123",
+                ["update_items"] = new JArray
+                {
+                    new JObject
+                    {
+                        ["fieldid"] = 12024,
+                        ["value_string"] = "xxx"
+                    }
+                }
+            };
+
+            var param2 = new JObject
+            {
+                ["userid"] = "456",
+                ["update_items"] = new JArray
+                {
+                    new JObject
+                    {
+                        ["fieldid"] = 12024,
+                        ["value_string"] = "yyy"
+                    }
+                }
+            };
+
+            param1.Merge(param2);
+            var json = JsonConvert.SerializeObject(param1);
+            Console.WriteLine(json);
+
+            Assert.AreEqual("456", param1["userid"].ToString());
+            Assert.AreEqual(2, (param1["update_items"] as JArray).Count);
+        }
+
+        private class MyEntity
+        {
+            public decimal Prop1 { get; set; }
+            public int Prop2 { get; set; }
+        }
     }
 }
